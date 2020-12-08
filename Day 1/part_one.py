832a03a20b16240a1465d448950dec20c6f4ba8f
BogdaDev <75583120+BogdaDev@users.noreply.github.com>
2020-12-08 22:05:34 +0200

    Removed the comments

--- Day 1/part_one.py
+++ Day 1/part_one.py
@@ -1,18 +1,15 @@
-input = [] #Defining an array called input. So I can loop something.
+input = []
 
-# Getting the input array.
 with open("Day 1\input.txt", "r") as file:
     input = file.readlines()
     for i in range(len(input)):
         input[i] = int(input[i].replace("\n", "")) 
 
-
-#A function that is going to return a number.
 def checkForYear(input):  
     for i in input: 
-        for x in input:    #Those 2 loops are for getting two numbers and 
-            if i + x == 2020: #adding them together and check if they are equal to 2020. 
-                return i * x #Multiplying the two numbers together
+        for x in input:  
+            if i + x == 2020: 
+                return i * x 
 
 print(checkForYear(input)) 
 
